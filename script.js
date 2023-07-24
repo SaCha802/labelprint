194c01d23b6e011638088de92452ae26fff8228a
Sebastian Tylka <113390513+SaCha802@users.noreply.github.com>
2023-07-24 13:38:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-$(document).ready(function () {
-  // Function to send cURL request
+document.addEventListener("DOMContentLoaded", function () {
+  // Function to send cURL request using Axios
   function sendCurlRequest(curlCommand) {
-    $.ajax({
-      url: 'https://connect.squareup.com/v2/catalog/search-catalog-items',
-      crossDomain: true,
-      method: 'post',
-      headers: {
-        'Square-Version': '2023-07-20',
-        'Authorization': 'Bearer EAAAERHGm_O9boKOGgOBZHsPW9Kz19K4DWJuTOO6kAmmR13mn2IDh5E2Q8GwZfbF'
-      },
-      contentType: 'application/json',
-      // data: '{\n    "text_filter": "050000105229"\n  }',
-      data: JSON.stringify({
-        'text_filter': '050000105229'
+  axios.post(
+  'https://connect.squareup.com/v2/catalog/search-catalog-items',
+  // '{\n    "text_filter": "050000105229"\n  }',
+  {
+    'text_filter': '050000105229'
+  },
+  {
+    headers: {
+      'Square-Version': '2023-07-20',
+      'Authorization': 'Bearer EAAAERHGm_O9boKOGgOBZHsPW9Kz19K4DWJuTOO6kAmmR13mn2IDh5E2Q8GwZfbF',
+      'Content-Type': 'application/json'
+    }
+  }
+);
+      .then((response) => {
+        // Display the response in the responseContainer div
+        document.getElementById("responseContainer").innerText = JSON.stringify(response.data, null, 2);
       })
-    }).done(function(response) {
-      console.log(response);
-    });
+      .catch((error) => {
+        // Display the error in the responseContainer div
+        document.getElementById("responseContainer").innerText = "Error: " + JSON.stringify(error.response.data, null, 2);
+      });
   }
 
   // Event listener for the button click
-  $("#sendButton").on("click", function () {
-    var curlCommand = $("#curlInput").val();
+  document.getElementById("sendButton").addEventListener("click", function () {
+    var curlCommand = document.getElementById("curlInput").value;
     sendCurlRequest(curlCommand);
   });
 });
