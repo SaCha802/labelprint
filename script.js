df975c7abd7e883fa4b76b35b020044506fd3976
SaCha802 <stylk2@uic.edu>
2023-07-24 03:16:28 -0500

    4

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 $(document).ready(function () {
   // Function to send cURL request
-  function sendCurlRequest(sku) {
+  function sendCurlRequest(curlCommand) {
     $.ajax({
-      url: 'https://connect.squareupsandbox.com/v2/catalog/search-catalog-items',
+      url: 'https://cors-anywhere.herokuapp.com/https://connect.squareup.com/v2/catalog/search-catalog-items',
       crossDomain: true,
       method: 'post',
       headers: {
         'Square-Version': '2023-07-20',
-        'Access-Control-Allow-Origin: *',
         'Authorization': 'Bearer EAAAERHGm_O9boKOGgOBZHsPW9Kz19K4DWJuTOO6kAmmR13mn2IDh5E2Q8GwZfbF'
       },
       contentType: 'application/json',
-      // data: '{\n    "text_filter": "050000105229",\n    "sort_order": "DESC",\n    "limit": 1\n  }',
+      // data: '{\n    "text_filter": "050000105229"\n  }',
       data: JSON.stringify({
-        'text_filter': sku,
-        'sort_order': 'DESC',
-        'limit': 1
+        'text_filter': '050000105229'
       })
     }).done(function(response) {
       console.log(response);
