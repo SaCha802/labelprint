1731890490cca9096297fc601537b46682f7cb90
Sebastian Tylka <113390513+SaCha802@users.noreply.github.com>
2023-07-25 02:47:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Function to send cURL request using Axios
-  function sendCurlRequest(sku, token) {
-  axios.post(
-  'https://corsproxy.io/?https://connect.squareup.com/v2/catalog/search-catalog-items',
-  // '{\n    "text_filter": sku\n  }',
-  {
-    'text_filter': sku,
-  },
-  {
-    headers: {
-      'Square-Version': '2023-07-20',
-      'Authorization': 'Bearer ${token}',
-      'Content-Type': 'application/json'
+    // Function to send cURL request using Axios
+    function sendCurlRequest(sku, token) {
+        axios.post(
+            'https://your-cors-proxy-url.com/https://connect.squareup.com/v2/catalog/search-catalog-items',
+            {
+                'text_filter': sku,
+            },
+            {
+                headers: {
+                    'Square-Version': '2023-07-20',
+                    'Authorization': `Bearer ${token}`, // Use backticks for string interpolation
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+            .then((response) => {
+                // Display the response in the responseContainer div
+                document.getElementById("responseContainer").innerText = JSON.stringify(response.data, null, 2);
+            })
+            .catch((error) => {
+                // Display the error in the responseContainer div
+                document.getElementById("responseContainer").innerText = "Error: " + JSON.stringify(error.response.data, null, 2);
+            });
     }
-  }
-)
-      .then((response) => {
-        // Display the response in the responseContainer div
-        document.getElementById("responseContainer").innerText = JSON.stringify(response.data, null, 2);
-      })
-      .catch((error) => {
-        // Display the error in the responseContainer div
-        document.getElementById("responseContainer").innerText = "Error: " + JSON.stringify(error.response.data, null, 2);
-      });
-  }
 
-  // Event listener for the button click
-  document.getElementById("sendButton").addEventListener("click", function () {
-    var token = document.getElementById("tokenInput").value;
-    var sku = document.getElementById("skuInput").value;
-    sendCurlRequest(sku, token);
-  });
+    // Event listener for the button click
+    document.getElementById("sendButton").addEventListener("click", function () {
+        var token = document.getElementById("tokenInput").value;
+        var sku = document.getElementById("skuInput").value;
+        sendCurlRequest(sku, token);
+    });
 });
